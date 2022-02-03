2b93a423ccb7a4807e6e8efb9fa37f9798607298
AJKAR <karczewskiamanda@gmail.com>
2022-02-02 19:52:26 -0600

    create cache elements

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('sanity check')
+// 3 cached element references with IDs
+// input, button, ul
+
+const inputElement = document.getElementById('input')
+
+const buttonElement = document.getElementById('submit-button')
+
+const ulElement = document.getElementById('todo-list')
